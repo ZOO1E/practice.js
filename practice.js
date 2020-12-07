e779a874076a09e6a9b0458733095ba3bd873014
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-07 23:12:01 +0900

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,33 @@
+const country = 'korea';
+const continent = 'Asia';
+const population = '70';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+ language = 'korean';
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+console.log(population/2);
+population++;
+console.log(population > 6);
+console.log(population < 33);
+
+const description = 
+country +
+ 'is in ' +
+continent +
+ ', and its ' +
+  population + 
+  'million people speak' + 
+  language;
+  console.log(description);
+
+
+
+
+
