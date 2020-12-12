b3814f4a9922053ab04ca87e2a0b5b59a7b08781
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-12 10:30:28 +0900

    Add files via upload

--- practice.js
+++ practice.js
@@ -13,7 +13,6 @@ console.log(typeof population);
 console.log(typeof country);
 
 console.log(population/2);
-population++;
 console.log(population > 6);
 console.log(population < 33);
 
@@ -26,6 +25,24 @@ continent +
   'million people speak' + 
   language;
   console.log(description);
+
+  //Lecture:Conversion and Coercion
+const first ='9'-'5'; //4
+const second  = '19' - '13' + '17'; //617
+const third = '19' - '13' + 17; //23
+const forth = '123' < 57; //false
+const fifth = 5 + 6 + '4' + 9 - 4 - 2; // 1143
+console.log(first , second , third , forth , fifth);
+ 
+//Lecture :Equality Operators: == vs ===
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+if (numNeighbours === 1) { //cuz === accept 1 to string so we must use number func!
+    console.log('Only 1 border!');
+}else if (numNeighbours > 1){
+    console.log('More than 1 border in case');
+}else{
+    console.log('No borders');
+}
 
 
 
